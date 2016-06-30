2bc803616670162524e3039278cab01db0b2b392
jessonfoo <jessonfoo@gmail.com>
2016-06-30 12:52:24 -0700

    added dev option: dev option uses var test as app controller.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+var dev = true;
 function gameCanvas() {
 	this.fps = 30;
 	this.canvas = document.getElementById('space-invaders');
 	this.width = 600;
 	this.height= 375;
+	this.minStarSpeed= 15;
+	this.maxStarSpeed= 30;
+	this.bgStars = 10;
 	this.intervalId = 0;
 }
 
@@ -36,5 +40,34 @@ gameCanvas.prototype.draw = function() {
  	ctx.fillStyle = '#000000';
 	ctx.fillRect(0, 0, this.width, this.height);
 };
-var test = new gameCanvas();
+gameCanvas.prototype.start = function() {
+	var self = this;
+	//	Start the timer.
+	this.intervalId = setInterval(function() {
+    console.log('redraw');
+		self.update();
+		self.draw();	
+	}, 1000 / this.fps);
+};
 
+gameCanvas.prototype.stop = function() {
+  console.log('stop');
+	clearInterval(this.intervalId);
+};
+
+gameCanvas.prototype.update = function() {
+  console.log('update');
+};
+
+if(dev == true){
+  var test = new gameCanvas();
+  test.canvas.addEventListener('mouseover',function(e){
+    e.preventDefault();
+    test.start();
+  });
+  test.canvas.addEventListener('mouseleave',function(e){
+    e.preventDefault();
+    test.stop();
+  });
+
+}
