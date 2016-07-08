4e836480ae97cd4ea9ebc8e9b382080d4e591efc
jessonfoo <jessonfoo@gmail.com>
2016-07-07 23:26:04 -0700

    diverging

--- js/main.js
+++ js/main.js
@@ -1,13 +1,48 @@
 var CANVAS_WIDTH = 600;
 var CANVAS_HEIGHT = 375;
-var canvas, ctx;
-canvas = document.createElement('canvas');
-ctx = canvas.getContext("2d");
+var SHIP= new Image();
+var LEFT_KEY = 37;
+var RIGHT_KEY = 39;
+var SHOOT_KEY = 32;
+var canvas, ctx, ship, shipSrc, shipX, shipY,invaderSrc,invaders,invaderImg,shipImg;
+shipY=300;
+shipX=300;
+var initCanvas = function() {
+  canvas = document.getElementById('gameCanvas');
+  canvas.width = 640;
+  canvas.height= 375;
+  ctx = canvas.getContext('2d');
+  ctx.fillStyle='#000';
+  draw(CANVAS_WIDTH,CANVAS_HEIGHT,drawBG);
+  shipImg= new Image();
+  shipImg.src = 'img/ship.png';
+  invaderImg=new Image();
+  invaderImg.src = 'img/invader.gif';
+  setImages();
+  window.onkeydown=function(e){
+    switch(e.keyCode){
+      case 37: // left
 
-function createCanvas(){
+      break;
+      case 39: // left
+      break;
+
+    }
+  }
+}
+var setImages = function() {
   var self = this;
-  self.canvas = canvas;
-	this.ctx = this.canvas.getContext('2d');
+  canvas = draw(2, 2, function(ctx) {
+      ctx.fillStyle = '#000';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    });
+    bulletImg = new Image();
+    bulletImg.src = canvas.toDataURL();
+}
+var drawBG = function (ctx){
+  var self = this;
+  this.canvas = canvas;
+	ctx = this.canvas.getContext('2d');
 	this.width = 600;
 	this.height = 275;
   canvas.width = this.width;
@@ -15,16 +50,26 @@ function createCanvas(){
 	ctx.fillStyle='#000';
   ctx.rect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   ctx.fill();
-  centerText('Space Invaders',CANVAS_WIDTH/2,CANVAS_HEIGHT/2.5,'#fff',20);
+  centerText('Space Invaders',CANVAS_WIDTH/2,CANVAS_HEIGHT/2.5,'#fff',28);
   //print('space invaders')
   return canvas;
 }
-function print(text,x,y,color,size){
-  if (typeof size !== 'undefined') ctx.font = size + "px Megrim";
+// function for drawing things on canvas
+var draw = function (width, height, draw) {
+  var canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  var ctx = canvas.getContext('2d');
+  draw(ctx);
+  return canvas;
+}
+var print = function (text,x,y,color,size){
+  // console.log(ctx.font);
   if (typeof color !== 'undefined') ctx.fillStyle = color;
+  ctx.font = size + "px Megrim";
   ctx.fillText(text,x,y);
 }
-function centerText(text, x, y, color, fs) {
+var centerText = function (text, x, y, color, fs) {
    var textWidth= ctx.measureText(text);
   print(text, x - textWidth.width, y, color, fs);
 }
@@ -34,9 +79,26 @@ function centerText(text, x, y, color, fs) {
 function p(x){
   console.log(x);
 }
+$(document).ready(function(){
+// ctx = canvas.getContext("2d");
 // p(createCanvas());
-var x = createCanvas();
-
-document.body.appendChild(x);
-p(canvas);
-// document.body.append(createCanvas());
+window.onkeydown=function(e){
+  switch(e.keyCode){
+    case 37: // left
+    break;
+    case 39: // left
+    break;
+  }
+}
+  initCanvas();
+  setImages();
+// window.onload=function(){
+//   // var x = createCanvas();
+//   initCanvas();
+//   setImages();
+//   // document.body.appendChild(x);
+// // }
+// // document.body.append(createCanvas());
+//
+// }
+})
